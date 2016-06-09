1e3b7b710ef62d3e509c45bb210292d8833466f2
dgilbs <danielgilberg@gmail.com>
2016-06-09 14:36:50 -0400

    first commit

---
+++ CoinChange.rb
@@ -0,0 +1,70 @@
+require 'pry'
+
+# Write an algorithm that would show all the combinations of coins that equal a dollar
+# Need to go through 
+
+#get combinations with each individually
+
+
+#[{q: 4}, {d: 10}]
+
+
+class CoinChange
+
+  attr_accessor :coin_purse
+  attr_accessor :combinations
+
+  def initialize
+    @coin_purse = {q: 25, d: 10, n: 5, p: 1}
+    @combinations = []
+  end
+
+  def sum_of_combo(hash)
+    sum = 0
+    coins = {q: 25, d: 10, n: 5, p: 1}
+    hash.each do |key, amount|
+      sum += (amount * coins[key])
+    end
+    sum
+  end
+
+  def all_combinations
+    all_combos =[]
+    counter1 = 0
+    while counter1 <= (100/self.coin_purse[self.coin_purse.keys[0]])    
+      counter2=0
+      while counter2 <=(100/self.coin_purse[self.coin_purse.keys[1]])
+        counter3 = 0
+        while counter3 <=(100/self.coin_purse[self.coin_purse.keys[2]])
+          counter4 = 0
+          while counter4 <=(100/self.coin_purse[self.coin_purse.keys[3]])
+            combination = {}
+            combination[self.coin_purse.keys[0]] = counter1
+            combination[self.coin_purse.keys[1]] = counter2
+            combination[self.coin_purse.keys[2]] = counter3
+            combination[self.coin_purse.keys[3]] = counter4
+            counter4 += 1
+            all_combos.push(combination) if self.sum_of_combo(combination) == 100
+          end
+          counter3 += 1
+        end
+        counter2 +=1
+      end
+      counter1 += 1
+    end
+    all_combos
+  end
+
+
+
+end
+
+
+
+c = CoinChange.new
+
+binding.pry
+
+
+
+x = "hello"
---
+++ string_permutations.rb
@@ -0,0 +1,51 @@
+require "pry"
+
+
+def string_permutations(word)
+  letters = word.split("")
+  letter_hash = {}
+  total = factorial(word.length)
+  total_per = total/word.length
+  letters.each do |letter|
+    letter_hash[letter] = []
+  end
+  letter_hash.each do |letter, combination|
+    while combination.length < total_per
+      string = letter.to_s
+      new_arr = letters.reject{|element| element == letter}
+      find_combinations(new_arr, combination, string, letter)
+    end
+  end
+  letter_hash.values.flatten
+end
+
+def factorial(num)
+  product = 1 
+  counter = 1
+  while counter <= num
+    product *= counter
+    counter += 1
+  end
+  product
+end
+
+def find_combinations(array1, array2, string, letter)
+  if array1.length <= 1
+    string += array1[0]
+    array2.push(string) if !array2.include?(string)
+  else 
+    new_letter = array1.sample
+    string += new_letter 
+    binding.pry
+    # array1.delete_at(array1.index(letter) || array1.length)
+    new_arr = array1.reject{|element| element == new_letter} if string.include?(letter)
+    find_combinations(array1, array2, string, letter)
+  end
+end
+
+
+x = string_permutations("abc")
+
+binding.pry
+
+x = 1
