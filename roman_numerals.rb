8e6ee9194a6842fb645ce23fb0a353d6dad5b304
dgilbs <danielgilberg@gmail.com>
2016-06-13 13:57:52 -0400

    changes library to class variable and method

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -11,16 +11,19 @@ class RomanNumeral
   attr_accessor :library, :numerals
 
   def initialize(numerals)
-    @library = {i: 1, v: 5, x: 10, l: 50, c: 100, d: 500}
     @numerals = numerals
   end
 
   def compare_numerals (sym1, sym2)
-    self.library[sym1.to_sym] > self.library[sym2.to_sym]
+    self.class.library[sym1.to_sym] > self.class.library[sym2.to_sym]
   end
 
   def length
     self.numerals.length
+  end
+
+  def self.library
+    library = {i: 1, v: 5, x: 10, l: 50, c: 100, d: 500}
   end
 
 
@@ -32,10 +35,10 @@ class RomanNumeral
       first_num = letters[counter]
       second_num = letters[counter + 1]
       if second_num && first_num != second_num && !self.compare_numerals(first_num, second_num)
-        sum += self.library[second_num.to_sym] - self.library[first_num.to_sym]
+        sum += self.class.library[second_num.to_sym] - self.class.library[first_num.to_sym]
         counter += 2
       else
-        sum += self.library[first_num.to_sym]
+        sum += self.class.library[first_num.to_sym]
         counter += 1
       end
     end
