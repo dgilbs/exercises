7aa917306c77bd437eab494706a00c8be2458202
dgilbs <danielgilberg@gmail.com>
2016-06-18 19:18:53 -0400

    adds letter integer string challenge

---
+++ letter_integer_string.rb
@@ -0,0 +1,10 @@
+require 'pry'
+
+#Challenge is to take a string that varies between letters and numbers, consistently by the same number
+#the string can be "1a2b", "12ab" and so on--it will always vary between letters and numbers, but will always have the same number
+#goal is to take one of those strings and to return an array that creates sub strings of each set of numbers of letters
+#i.e. result of "123abc456def" would be ["123", "abc", "456", "def"]
+
+
+class LetterIntegerString
+end
